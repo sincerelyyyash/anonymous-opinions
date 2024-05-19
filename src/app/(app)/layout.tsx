4f904f4b064d654d710843d4deef2ba44a1b5c32
sincerelyyyash <yta0088@gmail.com>
2024-05-20 03:17:15 +0530

    Message card component added

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import AuthProvider from "@/context/AuthProvider";
-import { Toaster } from "@/components/ui/toaster"
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
       <body className={inter.className}>
+        <Navbar/>
         {children}
-        <Toaster />
       </body>
-      </AuthProvider>
     </html>
   );
 }
---
+++ src/components/MessageCard.tsx
@@ -0,0 +1,77 @@
+"use client"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+
+  import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog"
+import { Button } from "./ui/button"
+import { X } from "lucide-react"
+import { Message } from "@/model/user.model"
+import { useToast } from "./ui/use-toast"
+import axios from "axios"
+import { ApiResponse } from "@/types/ApiResponse"
+  
+  
+type MessageCardProps = {
+    message: Message,
+    onMessageDelete: (messageId: string) => void
+}
+
+
+const MessageCard = ({message, onMessageDelete}: MessageCardProps) => {
+    const { toast } = useToast()
+
+    const handleDeleteConfirm = async () =>{
+        const response = await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`)
+        toast({
+            title: response.data.message
+        })
+        onMessageDelete(message._id)
+    }
+
+  return (
+    <Card>
+        <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+
+            <AlertDialog>
+        <AlertDialogTrigger asChild>
+                <Button variant="destructive"><X className="w-5 h-5" /> </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete the message.
+                </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+        </CardHeader>
+    </Card> 
+
+  )
+}
+
+
+
+export default MessageCard
