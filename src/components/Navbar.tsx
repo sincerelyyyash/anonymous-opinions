add45b3e35055a9047a4ec25f80cc11a31513410
sincerelyyyash <yta0088@gmail.com>
2024-05-27 23:42:57 +0530

    updated navbar color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     const user: User = session?.user as User
 
   return (
-    <nav className='p-4 md:p-6 shadow-md'>
+    <nav className='p-4 md:p-6 shadow-md bg-black/[0.60]'>
         <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
             <a 
             className='text-xl font-bold mb-4 md:mb-0'
