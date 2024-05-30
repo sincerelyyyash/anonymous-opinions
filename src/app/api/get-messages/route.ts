362f8c1acf408ebdc22c5a29b6ebc768b9070304
sincerelyyyash <yta0088@gmail.com>
2024-05-30 12:21:19 +0530

    Imports corrected

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -2,50 +2,50 @@ import { getServerSession } from "next-auth";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/user.model";
 import { User } from 'next-auth';
-import { authOptions } from "../signup/auth/[...nextauth]/options";
+import { authOptions } from "../auth/[...nextauth]/options";
 import mongoose from "mongoose";
 
 
-export async function GET(request: Request){
-    await dbConnect()
+export async function GET(request: Request) {
+  await dbConnect()
 
-    const session = await getServerSession(authOptions)
-    const user: User = session?.user as User
-    
-    if(!session || !session.user){
-        return Response.json({
-            success: false,
-            message: "Not authenticated"
-        },{status:401})
+  const session = await getServerSession(authOptions)
+  const user: User = session?.user as User
+
+  if (!session || !session.user) {
+    return Response.json({
+      success: false,
+      message: "Not authenticated"
+    }, { status: 401 })
+  }
+
+  const userId = new mongoose.Types.ObjectId(user._id);
+
+  try {
+    const user = await UserModel.aggregate([
+      { $match: { id: userId } },
+      { $unwind: '$messages' },
+      { $sort: { 'messages.createdAt': -1 } },
+      { $group: { _id: '$_id', messages: { $push: '$messages' } } }
+    ])
+
+    if (!user || user.length === 0) {
+      return Response.json({
+        success: false,
+        message: "User not found"
+      }, { status: 401 })
     }
 
-    const userId = new mongoose.Types.ObjectId(user._id);
+    return Response.json({
+      success: true,
+      message: user[0].messages
+    }, { status: 200 })
 
-    try {
-        const user = await UserModel.aggregate([
-            {$match: {id: userId}},
-            {$unwind: '$messages'},
-            {$sort: {'messages.createdAt': -1}},
-            {$group: {_id: '$_id', messages: {$push: '$messages'}}}
-        ])
-
-        if(!user || user.length ===0 ){
-            return Response.json({
-                success: false,
-                message: "User not found"
-            },{status:401})
-        }
-
-        return Response.json({
-            success: true,
-            message: user[0].messages
-        },{status: 200})
-
-    } catch (error) {
-        console.log("Error in getting messages", error)
-        return Response.json({
-            success:false,
-            message: "Error in getting messages"
-        },{status: 500})
-    }
+  } catch (error) {
+    console.log("Error in getting messages", error)
+    return Response.json({
+      success: false,
+      message: "Error in getting messages"
+    }, { status: 500 })
+  }
 }
