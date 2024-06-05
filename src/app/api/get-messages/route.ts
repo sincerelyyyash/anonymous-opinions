95b17f1375f297980e0bc8a279d301c35e7b1e94
sincerelyyyash <yta0088@gmail.com>
2024-06-05 23:54:13 +0530

    Route updated

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -4,48 +4,49 @@ import UserModel from "@/model/user.model";
 import { User } from 'next-auth';
 import { authOptions } from "../auth/[...nextauth]/options";
 import mongoose from "mongoose";
-
+import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-  await dbConnect()
+  await dbConnect();
 
-  const session = await getServerSession(authOptions)
-  const user: User = session?.user as User
+  const session = await getServerSession(authOptions);
 
   if (!session || !session.user) {
-    return Response.json({
+    return NextResponse.json({
       success: false,
       message: "Not authenticated"
-    }, { status: 401 })
+    }, { status: 401 });
   }
 
-  const userId = new mongoose.Types.ObjectId(user._id);
+  const user: User = session.user as User;
+  const userId = new mongoose.Types.ObjectId(user.id);
 
   try {
-    const user = await UserModel.aggregate([
-      { $match: { id: userId } },
+    const userMessages = await UserModel.aggregate([
+      { $match: { _id: userId } },
       { $unwind: '$messages' },
       { $sort: { 'messages.createdAt': -1 } },
       { $group: { _id: '$_id', messages: { $push: '$messages' } } }
-    ])
+    ]);
 
-    if (!user || user.length === 0) {
-      return Response.json({
+    if (!userMessages || userMessages.length === 0) {
+      return NextResponse.json({
         success: false,
         message: "User not found"
-      }, { status: 401 })
+      }, { status: 404 });
     }
 
-    return Response.json({
+    return NextResponse.json({
       success: true,
-      message: user[0].messages
-    }, { status: 200 })
+      messages: userMessages[0].messages
+    }, { status: 200 });
 
   } catch (error) {
-    console.log("Error in getting messages", error)
-    return Response.json({
+    console.log("Error in getting messages", error);
+    return NextResponse.json({
       success: false,
       message: "Error in getting messages"
-    }, { status: 500 })
+    }, { status: 500 });
   }
 }
+
