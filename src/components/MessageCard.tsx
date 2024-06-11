9b3dd214377069845c520027bd4e8b376a4729c7
sincerelyyyash <yta0088@gmail.com>
2024-06-11 19:43:06 +0530

    MessageCard updated

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -1,74 +1,73 @@
-"use client"
+"use client";
 import {
-    Card,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 
-  import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog"
-import { Button } from "./ui/button"
-import { X } from "lucide-react"
-import { Message } from "@/model/user.model"
-import { useToast } from "./ui/use-toast"
-import axios from "axios"
-import { ApiResponse } from "@/types/ApiResponse"
-  
-  
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
+import { X } from "lucide-react";
+import { Message } from "@/model/user.model";
+import { useToast } from "./ui/use-toast";
+import axios from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+
 type MessageCardProps = {
-    message: Message,
-    onMessageDelete: (messageId: string) => void
-}
+  message: Message;
+  onMessageDelete: (messageId: string) => void;
+};
 
+const MessageCard = ({ message, onMessageDelete }: MessageCardProps) => {
+  const { toast } = useToast();
 
-const MessageCard = ({message, onMessageDelete}: MessageCardProps) => {
-    const { toast } = useToast()
-
-    const handleDeleteConfirm = async () =>{
-        const response = await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`)
-        toast({
-            title: response.data.message
-        })
-        onMessageDelete(message._id)
-    }
+  const handleDeleteConfirm = async () => {
+    const response = await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`);
+    toast({
+      title: response.data.message,
+    });
+    onMessageDelete(message._id);
+  };
 
   return (
     <Card>
-        <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-
-            <AlertDialog>
-        <AlertDialogTrigger asChild>
-                <Button variant="destructive"><X className="w-5 h-5" /> </Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                <AlertDialogDescription>
-                    This action cannot be undone. This will permanently delete the message.
-                </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
+      <CardHeader className="relative">
+        <CardTitle>{message.content}</CardTitle>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button variant="destructive" className="absolute top-0 right-0 mt-2 mr-2">
+              <X className="w-5 h-5" />
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete the message.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
         </AlertDialog>
-        </CardHeader>
-    </Card> 
+      </CardHeader>
+      <CardFooter></CardFooter>
+    </Card>
+  );
+};
 
-  )
-}
-
-
-
-export default MessageCard
+export default MessageCard;
