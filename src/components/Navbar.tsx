27f793badd9482aff342bf258316a988f9f5e0de
sincerelyyyash <yta0088@gmail.com>
2024-06-11 23:38:58 +0530

    Navbar updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,34 +2,40 @@
 import React from 'react'
 import Link from 'next/link'
 import { useSession, signOut } from 'next-auth/react'
-import { User } from 'next-auth'
 
 
 const Navbar = () => {
-    const { data: session} = useSession()
-    const user: User = session?.user as User
+  const { data: session } = useSession()
 
   return (
     <nav className='p-4 md:p-6 shadow-md bg-black/[0.60]'>
-        <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
-            <a 
-            className='text-xl font-bold mb-4 md:mb-0'
-            href="#">Anonymous Opinions</a>
-            {
-                session ? (
-                    <>
-                    <span className='mr-4'>Welcome, {user.username || user.email}</span>
-                    <button className='w-full md:w-auto' onClick={()=>{
-                        signOut()
-                    }}>Logout</button>
-                    </> 
-                ) : (
-                    <Link href={'/signin'}>
-                        <button className='w-full md:w-auto' >Log In</button>
-                    </Link>
-                )
-            }
-        </div>
+      <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
+        <a
+          className='text-xl md:mt-0
+            font-bold bg-clip-text text-transparent 
+            bg-gradient-to-b from-neutral-50 to-neutral-400"font-bold mb-4 md:mb-0'
+          href="#">Anonymous Opinions</a>
+        {
+          session ? (
+            <>
+              <Link href={'dashboard'}>
+                <span className='font-bold bg-clip-text text-transparent 
+            bg-gradient-to-b from-neutral-200 to-neutral-50"font-bold mb-4 md:mb-0'>Dashboard</span>
+              </Link>
+
+              <button className='font-bold bg-clip-text text-transparent 
+            bg-gradient-to-b from-neutral-50 to-neutral-400"font-bold mb-4 md:mb-0' onClick={() => {
+                  signOut()
+                }}>Logout</button>
+            </>
+          ) : (
+            <Link href={'/signin'}>
+              <button className='font-bold bg-clip-text text-transparent 
+            bg-gradient-to-b from-neutral-50 to-neutral-400"font-bold mb-4 md:mb-0' >Log In</button>
+            </Link>
+          )
+        }
+      </div>
     </nav>
   )
 }
